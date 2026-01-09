4d0f8b42bde73466932d4577b529f9462cc6a890
Hassib Kevin Ezzedine <136286981+axo47@users.noreply.github.com>
2026-01-08 20:10:50 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/new-portfolio/", // <--- AJOUTE CETTE LIGNE ICI
+  base: "/",
   server: {
     host: "::",
     port: 8080,
